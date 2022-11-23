10661bf1db31597ceef7b051740059727242f2b4
Gaute Lorentsen <gudelo97@gmail.com>
2022-11-23 11:33:29 +0100

    Fixed issue where reference inside of reference wasn't populated

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,7 +3,22 @@ const Workout = require('../models/workout');
 // Display list of all workouts
 const workout_list = async (req, res, next) => {
   try {
-    const list_workouts = await Workout.find().populate('excercises');
+    const list_workouts = await Workout.find()
+      .populate([
+        {
+          path: 'excercises',
+          populate: { path: 'body_part' },
+        },
+      ])
+      .populate([
+        {
+          path: 'excercises',
+          populate: { path: 'category' },
+        },
+      ]);
+    // .populate('excercises')
+    // .populate('category')
+    // .populate('body_part');
 
     return res.json(list_workouts);
   } catch (err) {
